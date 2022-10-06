43ba113110868bccf859ec8d1a5870fb34d39d07
robikasep <robikasep@hotmail.com>
2022-10-06 07:05:31 -0700

    add constructor for protect

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -11,16 +11,13 @@ use App\Models\User;
 class Admin extends BaseController
 {
 
-
     public function __construct()
     {
         $this->db = \Config\Database::connect();
 
         if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
+            echo '<h1> Akses Ditolak </h1>';
             exit;
-        } else {
-            return view('user_normal/tentang');
         }
     }
 
@@ -28,109 +25,78 @@ class Admin extends BaseController
 
     public function admintest()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            return view('/admin/adminview');
-        }
+
+        return view('/admin/adminview');
+
     }
 
     public function index()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            return view('admin/adminview');
-        }
+
+        return view('admin/adminview');
+
     }
 
     public function github()
     {
         // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            return view('admin/github');
-        }
+
+        return view('admin/github');
+
     }
 
     public function tentang()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            return view('admin/tentang');
-        }
-
+        return view('admin/tentang');
     }
 
 
     public function ajaxLoadData()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            $db = db_connect();
-            $builder = $db->table('user')
-            ->select('id_number ,username, email, level.name_level_user')
-            ->join('level', 'user.name_level_user_id = level.id','left');
+
+        $db = db_connect();
+        $builder = $db->table('user')
+        ->select('id_number ,username, email, level.name_level_user')
+        ->join('level', 'user.name_level_user_id = level.id','left');
 
     // alternatif ajax edit
-            return DataTable::of($builder)
-            ->edit('id_number', function($row){
-                return '
-                <a href="admin/edit/'.$row->id_number.' " class="btn btn-info btn-sm"><i class="fas fa-pencil-alt"></i></a>
-                <a href="admin/delete/'.$row->id_number.' " class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
-                ';
-            })
-            ->toJson(true);
-        }
+        return DataTable::of($builder)
+        ->edit('id_number', function($row){
+            return '
+            <a href="admin/edit/'.$row->id_number.' " class="btn btn-info btn-sm"><i class="fas fa-pencil-alt"></i></a>
+            <a href="admin/delete/'.$row->id_number.' " class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
+            ';
+        })
+        ->toJson(true);
+        
         
     }
 
 
     public function adminPagePost()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            $data['level'] = $this->db->table('level')->select('id, name_level_user')->get()->getResult();
+        $data['level'] = $this->db->table('level')->select('id, name_level_user')->get()->getResult();
 
-            return view('/admin/create_user', $data);
-        }
-        
+        return view('/admin/create_user', $data);
+
+
     }
 
 
     public function adminPost()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            $model = new User();
-            $data = [
-                'username' => $this->request->getVar('username'),
-                'email'  => $this->request->getVar('email'),
-                'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-                'name_level_user_id'  => $this->request->getVar('name_level_user_id'),
-            ];
-            $model->insert($data);
-            return $this->response->redirect(site_url('/admin'));
-        }
-        
+
+        $model = new User();
+        $data = [
+            'username' => $this->request->getVar('username'),
+            'email'  => $this->request->getVar('email'),
+            'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+            'name_level_user_id'  => $this->request->getVar('name_level_user_id'),
+        ];
+        $model->insert($data);
+        return $this->response->redirect(site_url('/admin'));
+
+
     }
 
 
@@ -138,75 +104,54 @@ class Admin extends BaseController
     
     public function adminDelete($id = null)
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            $model = new User();
-            $data['user'] = $model->where('id_number', $id)->delete($id);
-            return $this->response->redirect(site_url('/admin'));
-        }
-        
+
+        $model = new User();
+        $data['user'] = $model->where('id_number', $id)->delete($id);
+        return $this->response->redirect(site_url('/admin'));
+
+
     }
 
 
     public function adminUpdate()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            $item = new User();
-            $id = $this->request->getVar('id_number');
-            $data = [
-                'username' => $this->request->getVar('username'),
-                'email'  => $this->request->getVar('email'),
-                'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-                'name_level_user_id'  => $this->request->getVar('name_level_user_id'),
-            ];
-            $item->update($id, $data);
-            return $this->response->redirect(site_url('/admin'));
-        }
-        
+        $item = new User();
+        $id = $this->request->getVar('id_number');
+        $data = [
+            'username' => $this->request->getVar('username'),
+            'email'  => $this->request->getVar('email'),
+            'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+            'name_level_user_id'  => $this->request->getVar('name_level_user_id'),
+        ];
+        $item->update($id, $data);
+        return $this->response->redirect(site_url('/admin'));
     }
     
 
     public function getEdit($id_number)
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-            $model = new User();
-            $data['user'] = $model->getWhere(['id_number' => $id_number])->getRow();
-            $data['level'] = $this->db->table('level')->select('id, name_level_user')->get()->getResult();
+
+        $model = new User();
+        $data['user'] = $model->getWhere(['id_number' => $id_number])->getRow();
+        $data['level'] = $this->db->table('level')->select('id, name_level_user')->get()->getResult();
 
 
-            echo view('/admin/edit_user', $data);
-        }
+        echo view('/admin/edit_user', $data);
 
     }
 
     public function getCategory()
     {
-        // cek session apakah admin atau tidak
-        if (session()->get('name_level_user_id') != "1") {
-            return view('public/not_login');
-            exit;
-        } else {
-         $db      = \Config\Database::connect();
-         $builder = $db->table('level');
-         $query   = $builder->get();
 
-         foreach ($query->getResult() as $row) {
-            echo $row->id;
-            echo $row->name_level_user;
-        }
+     $db      = \Config\Database::connect();
+     $builder = $db->table('level');
+     $query   = $builder->get();
+
+     foreach ($query->getResult() as $row) {
+        echo $row->id;
+        echo $row->name_level_user;
     }
-    
+
 }
 
 
--- app/Controllers/UserNormal.php
+++ app/Controllers/UserNormal.php
@@ -12,131 +12,98 @@ class UserNormal extends BaseController
 	public function __construct()
 	{
 		$this->db = \Config\Database::connect();
+
+		if (session()->get('name_level_user_id') != "2") {
+			echo '<h1> Akses Ditolak </h1>';
+			exit;
+		}
+
 	}
 
 
 	public function usertest()
 	{
-        // cek session apakah admin atau tidak
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			return view('user_normal/tentang');
-		}
+		return view('user_normal/tentang');
 	}
 
 
 	public function index()
 	{
-		// cek session apakah admin atau tidak
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			return view('user_normal/userview');
-		}
-		
+		return view('user_normal/userview');
 	}
 
 	public function github()
 	{
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			return view('user_normal/github');
-		}
+		return view('user_normal/github');
 	}
 
 	public function tentang()
 	{
-		// cek session apakah admin atau tidak
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			return view('user_normal/tentang');
-		}
+		return view('user_normal/tentang');
+
 	}
 
 	public function ajaxLoadData()
 	{
-
-		// cek session apakah admin atau tidak
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			$db = db_connect();
-			$builder = $db->table('user')
-			->select('id_number ,username, email, level.name_level_user')
-			->join('level', 'user.name_level_user_id = level.id','left')
-			->where('name_level_user', 'user');
+		$db = db_connect();
+		$builder = $db->table('user')
+		->select('id_number ,username, email, level.name_level_user')
+		->join('level', 'user.name_level_user_id = level.id','left')
+		->where('name_level_user', 'user');
 
     // alternatif ajax edit
-			return DataTable::of($builder)
-			->edit('id_number', function($row){
-				return '
-				<a href="user/edit/'.$row->id_number.' " class="btn btn-info btn-sm"><i class="fas fa-pencil-alt"></i></a>
-				<a href="user/delete/'.$row->id_number.' " class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
-				';
-			})
-			->toJson(true);
-		}
+		return DataTable::of($builder)
+		->edit('id_number', function($row){
+			return '
+			<a href="user/edit/'.$row->id_number.' " class="btn btn-info btn-sm"><i class="fas fa-pencil-alt"></i></a>
+			<a href="user/delete/'.$row->id_number.' " class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i></a>
+			';
+		})
+		->toJson(true);
+		
 		
 	}
 
 	public function userPagePost()
 	{
-		// cek session apakah admin atau tidak
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			$data['level'] = $this->db->table('level')->select('id, name_level_user')
-			->where('name_level_user', 'user')->get()->getResult();
 
-			return view('/user_normal/create_user', $data);
-		}
+		$data['level'] = $this->db->table('level')->select('id, name_level_user')
+		->where('name_level_user', 'user')->get()->getResult();
+
+		return view('/user_normal/create_user', $data);
+		
 		
 	}
 
 
 	public function userPost()
 	{
-		// cek session apakah admin atau tidak
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			$model = new User();
-			$data = [
-				'username' => $this->request->getVar('username'),
-				'email'  => $this->request->getVar('email'),
-				'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-				'name_level_user_id'  => $this->request->getVar('name_level_user_id'),
-			];
-			$model->insert($data);
-			return $this->response->redirect(site_url('/user'));
-		}	
+
+		$model = new User();
+		$data = [
+			'username' => $this->request->getVar('username'),
+			'email'  => $this->request->getVar('email'),
+			'password'  => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+			'name_level_user_id'  => $this->request->getVar('name_level_user_id'),
+		];
+		$model->insert($data);
+		return $this->response->redirect(site_url('/user'));
+
 	}
 
 
 	public function userDelete($id = null)
 	{
-		if (session()->get('name_level_user_id') != "2") {
-			return view('public/not_login');
-			exit;
-		} else {
-			$model = new User();
-			$data['user'] = $model->where('id_number', $id)->delete($id);
-			return $this->response->redirect(site_url('/user'));
-		}
+
+		$model = new User();
+		$data['user'] = $model->where('id_number', $id)->delete($id);
+		return $this->response->redirect(site_url('/user'));
+		
 	}
 
 
 	public function userUpdate(){
+
 		$item = new User();
 		$id = $this->request->getVar('id_number');
 		$data = [
@@ -147,6 +114,7 @@ class UserNormal extends BaseController
 		];
 		$item->update($id, $data);
 		return $this->response->redirect(site_url('/user'));
+		
 	}
 
 
@@ -157,8 +125,9 @@ class UserNormal extends BaseController
 		$data['level'] = $this->db->table('level')->select('id, name_level_user')
 		->where('name_level_user', 'user')->get()->getResult();
 
-
 		echo view('/user_normal/edit_user', $data);
+		
+		
 	}
 
 }
